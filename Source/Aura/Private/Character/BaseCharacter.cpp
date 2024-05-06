b924a4710dc4f20c328f3b838de6a80a1f9e0e5f
swordandspatula <139723909+swordandspatula@users.noreply.github.com>
2024-05-05 21:11:51 -0500

    idle animations

--- Source/Aura/Private/Character/BaseCharacter.cpp
+++ Source/Aura/Private/Character/BaseCharacter.cpp
@@ -9,6 +9,9 @@ ABaseCharacter::ABaseCharacter()
 
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 
